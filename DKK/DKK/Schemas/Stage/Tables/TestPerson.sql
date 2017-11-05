8a34e39acbb91976ba4eeb452c181d6f8dc6c204
Derik Hammer <derik@sqlhammer.com>
2017-11-05 14:32:04 -0500

    Initial commit

---
+++ DKK/DKK/Schemas/Stage/Tables/TestPerson.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [Stage].[TestPerson]
+(
+	[FirstName] NVARCHAR(60) NOT NULL
+	,[LastName] NVARCHAR(60) NOT NULL
+	,[Rank] TINYINT NOT NULL
+	,[Address] NVARCHAR(255) NULL
+	,[IsKata] BIT NOT NULL
+	,[IsSemiKnockdown] BIT NOT NULL
+	,[IsKnockdown] BIT NOT NULL
+)
