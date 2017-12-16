f287e684e3403e83183a8db325bced60609c3c58
Derik Hammer <derik@sqlhammer.com>
2017-12-16 11:09:19 -0500

    Added in unintentionally ignored files.

---
+++ DKK/DKK/Publish/Script.PostDeployment.sql
@@ -0,0 +1,37 @@
+﻿
+--Load static tables
+PRINT 'Loading Event.Division...'
+:r .\..\Schemas\Event\Data\Division.sql
+
+PRINT 'Loading Event.Rank...'
+:r .\..\Schemas\Event\Data\Rank.sql
+
+PRINT 'Loading Event.MatchType...'
+:r .\..\Schemas\Event\Data\MatchType.sql
+
+PRINT 'Loading Event.EventType...'
+:r .\..\Schemas\Event\Data\EventType.sql
+
+PRINT 'Loading Facility.MartialArtType...'
+:r .\..\Schemas\Facility\Data\MartialArtType.sql
+
+PRINT 'Loading Person.Title...'
+:r .\..\Schemas\Person\Data\Title.sql
+
+--PRINT 'Loading Stage.Registration...'
+--:r .\..\Schemas\Stage\Data\Registration.sql
+
+PRINT 'Loading Facility.FacilityType...'
+:r .\..\Schemas\Facility\Data\FacilityType.sql
+
+PRINT 'Loading Facility.Facility...'
+:r .\..\Schemas\Facility\Data\Facility.sql
+
+PRINT 'Loading Facility.Dojo...'
+:r .\..\Schemas\Facility\Data\Dojo.sql
+
+PRINT 'Loading Event.Event...'
+:r .\..\Schemas\Event\Data\Event.sql
+
+
+
