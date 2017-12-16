67d4441771731bee2e2dceb85fb156093e884989
Derik Hammer <derik@sqlhammer.com>
2017-12-16 10:07:30 -0500

    udfGetDivisionHeader and some renamings.

---
+++ DKK/DKK/Schemas/Event/Functions/udfGetDivisionHeader.sql
@@ -0,0 +1,28 @@
+﻿CREATE FUNCTION [Event].[udfGetDivisionHeader]
+(
+	@MatchTypeName nvarchar(30),
+	@EventId INT,
+	@IsSpecialConsideration BIT
+)
+RETURNS TABLE
+AS
+RETURN
+(
+	SELECT 
+		  m.MatchDisplayId
+		  ,m.SubDivisionId
+		  ,d.MinimumWeight_lb
+		  ,d.MaximumWeight_lb
+		  ,d.WeightClass
+		  ,d.Gender
+		  ,(SELECT Name FROM [Event].[Rank] WHERE RankId = d.MinimumLevelId) MinimumBelt
+		  ,(SELECT Name FROM [Event].[Rank] WHERE RankId = d.MaximumLevelId) MaximumBelt
+		  ,d.MinimumAge
+		  ,d.MaximumAge 
+	FROM [Event].[Division] d
+	INNER JOIN [Event].[Match] m ON m.DivisionId = d.DivisionId
+	INNER JOIN [Event].[MatchType] mt ON mt.MatchTypeId = m.MatchTypeId
+	WHERE mt.IsSpecialConsideration = @IsSpecialConsideration
+		AND m.EventId = @EventId
+		AND mt.[Name] = @MatchTypeName
+)
