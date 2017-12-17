64911b4a04528e81190f2530f4c3fd7c534f4d0f
Derik Hammer <derik@sqlhammer.com>
2017-12-16 23:08:26 -0500

    Completed Kata ScoreCard, pending print testing.

---
+++ DKK/DKK/Schemas/Event/Views/vwDivisionHeader.sql
@@ -0,0 +1,18 @@
+﻿CREATE VIEW [Event].[vwDivisionHeader]
+AS
+SELECT 
+		m.MatchDisplayId
+		,m.SubDivisionId
+		,d.MinimumWeight_lb
+		,d.MaximumWeight_lb
+		,d.WeightClass
+		,d.Gender
+		,(SELECT Name FROM [Event].[Rank] WHERE RankId = d.MinimumLevelId) MinimumBelt
+		,(SELECT Name FROM [Event].[Rank] WHERE RankId = d.MaximumLevelId) MaximumBelt
+		,d.MinimumAge
+		,d.MaximumAge 
+		,mt.Name MatchTypeName
+		,m.EventId
+FROM [Event].[Division] d
+INNER JOIN [Event].[Match] m ON m.DivisionId = d.DivisionId
+INNER JOIN [Event].[MatchType] mt ON mt.MatchTypeId = m.MatchTypeId
