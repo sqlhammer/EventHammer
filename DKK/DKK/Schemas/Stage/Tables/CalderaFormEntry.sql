25633c8a5fe6beec7664cb72c5294a68e00a7e63
Derik Hammer <derik@sqlhammer.com>
2018-03-25 17:24:18 -0400

    Create a process to directly pull registration form data from kanreikai.com to dkk database.

---
+++ DKK/DKK/Schemas/Stage/Tables/CalderaFormEntry.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [Stage].[CalderaFormEntry]
+(
+	form_id NVARCHAR(18) NOT NULL
+	,entry_id INT NOT NULL
+	,field_id NVARCHAR(20) NOT NULL
+	,slug NVARCHAR(255) NOT NULL
+	,[value] NVARCHAR(MAX) NOT NULL
+)
+GO
+CREATE CLUSTERED INDEX IX_Stage_CalderaFormEntry_slug
+ON Stage.CalderaFormEntry (slug);
+GO
