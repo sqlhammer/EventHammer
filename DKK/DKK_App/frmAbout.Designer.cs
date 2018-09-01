2840858c87857d4e205b0614e226d73d8a07b6f2
Derik Hammer <derik@sqlhammer.com>
2018-09-01 16:03:12 -0400

    Updated version info for major release #1.

--- DKK/DKK_App/frmAbout.Designer.cs
+++ DKK/DKK_App/frmAbout.Designer.cs
@@ -43,9 +43,10 @@
             this.pbSQLHammer.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             this.pbSQLHammer.Cursor = System.Windows.Forms.Cursors.Hand;
             this.pbSQLHammer.Image = global::DKK_App.Properties.Resources.powered_by_sqlhammer;
-            this.pbSQLHammer.Location = new System.Drawing.Point(52, 46);
+            this.pbSQLHammer.Location = new System.Drawing.Point(26, 24);
+            this.pbSQLHammer.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pbSQLHammer.Name = "pbSQLHammer";
-            this.pbSQLHammer.Size = new System.Drawing.Size(658, 162);
+            this.pbSQLHammer.Size = new System.Drawing.Size(329, 84);
             this.pbSQLHammer.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pbSQLHammer.TabIndex = 0;
             this.pbSQLHammer.TabStop = false;
@@ -55,19 +56,21 @@
             // 
             this.lblMajorVersion.AutoSize = true;
             this.lblMajorVersion.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblMajorVersion.Location = new System.Drawing.Point(52, 262);
+            this.lblMajorVersion.Location = new System.Drawing.Point(26, 136);
+            this.lblMajorVersion.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblMajorVersion.Name = "lblMajorVersion";
-            this.lblMajorVersion.Size = new System.Drawing.Size(361, 37);
+            this.lblMajorVersion.Size = new System.Drawing.Size(123, 20);
             this.lblMajorVersion.TabIndex = 1;
-            this.lblMajorVersion.Text = "Major Version: 0 (alpha)";
+            this.lblMajorVersion.Text = "Major Version: 1";
             // 
             // lblBuild
             // 
             this.lblBuild.AutoSize = true;
             this.lblBuild.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblBuild.Location = new System.Drawing.Point(52, 325);
+            this.lblBuild.Location = new System.Drawing.Point(26, 169);
+            this.lblBuild.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblBuild.Name = "lblBuild";
-            this.lblBuild.Size = new System.Drawing.Size(176, 37);
+            this.lblBuild.Size = new System.Drawing.Size(87, 20);
             this.lblBuild.TabIndex = 2;
             this.lblBuild.Text = "Build: 0.0.1";
             // 
@@ -76,9 +79,10 @@
             this.lnklblSourceCode.ActiveLinkColor = System.Drawing.Color.OliveDrab;
             this.lnklblSourceCode.AutoSize = true;
             this.lnklblSourceCode.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lnklblSourceCode.Location = new System.Drawing.Point(52, 447);
+            this.lnklblSourceCode.Location = new System.Drawing.Point(26, 232);
+            this.lnklblSourceCode.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lnklblSourceCode.Name = "lnklblSourceCode";
-            this.lnklblSourceCode.Size = new System.Drawing.Size(272, 37);
+            this.lnklblSourceCode.Size = new System.Drawing.Size(137, 20);
             this.lnklblSourceCode.TabIndex = 3;
             this.lnklblSourceCode.TabStop = true;
             this.lnklblSourceCode.Text = "Github Repository";
@@ -89,9 +93,10 @@
             this.lnklblContact.ActiveLinkColor = System.Drawing.Color.OliveDrab;
             this.lnklblContact.AutoSize = true;
             this.lnklblContact.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lnklblContact.Location = new System.Drawing.Point(52, 509);
+            this.lnklblContact.Location = new System.Drawing.Point(26, 265);
+            this.lnklblContact.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lnklblContact.Name = "lnklblContact";
-            this.lnklblContact.Size = new System.Drawing.Size(328, 37);
+            this.lnklblContact.Size = new System.Drawing.Size(165, 20);
             this.lnklblContact.TabIndex = 4;
             this.lnklblContact.TabStop = true;
             this.lnklblContact.Text = "Contact the developer";
@@ -101,17 +106,18 @@
             // 
             this.lblBuildDate.AutoSize = true;
             this.lblBuildDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblBuildDate.Location = new System.Drawing.Point(52, 386);
+            this.lblBuildDate.Location = new System.Drawing.Point(26, 201);
+            this.lblBuildDate.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblBuildDate.Name = "lblBuildDate";
-            this.lblBuildDate.Size = new System.Drawing.Size(173, 37);
+            this.lblBuildDate.Size = new System.Drawing.Size(87, 20);
             this.lblBuildDate.TabIndex = 5;
             this.lblBuildDate.Text = "Build Date:";
             // 
             // frmAbout
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(766, 589);
+            this.ClientSize = new System.Drawing.Size(383, 306);
             this.Controls.Add(this.lblBuildDate);
             this.Controls.Add(this.lnklblContact);
             this.Controls.Add(this.lnklblSourceCode);
@@ -120,6 +126,7 @@
             this.Controls.Add(this.pbSQLHammer);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "frmAbout";
