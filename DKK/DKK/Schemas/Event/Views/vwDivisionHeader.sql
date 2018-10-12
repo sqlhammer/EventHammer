31b4edb939f381e2d6f7887b02b18437818ecb87
Derik Hammer <derik@sqlhammer.com>
2018-10-12 16:29:33 -0400

    Bug fix for division number report due to last minute changes with the way divisions are used in the system.

--- DKK/DKK/Schemas/Event/Views/vwDivisionHeader.sql
+++ DKK/DKK/Schemas/Event/Views/vwDivisionHeader.sql
@@ -1,7 +1,6 @@
 ﻿CREATE VIEW [Event].[vwDivisionHeader]
 AS
-SELECT 
-		m.MatchDisplayId
+SELECT d.DivisionId MatchDisplayId
 		,m.SubDivisionId
 		,d.MinimumWeight_lb
 		,d.MaximumWeight_lb
@@ -16,4 +15,12 @@ SELECT
 		,mt.IsSpecialConsideration
 FROM [Event].[Division] d
 INNER JOIN [Event].[Match] m ON m.DivisionId = d.DivisionId
-INNER JOIN [Event].[MatchType] mt ON mt.MatchTypeId = m.MatchTypeId
+INNER JOIN [Event].[MatchType] mt 
+	ON mt.MatchTypeId = m.MatchTypeId
+		AND 
+		(
+			CASE WHEN d.IsKata = 1 THEN 'Kata' ELSE '' END = mt.Name
+			OR CASE WHEN d.IsWeaponKata = 1 THEN 'Weapon Kata' ELSE '' END = mt.NAME
+            OR CASE WHEN d.IsSemiKnockdown = 1 THEN 'Semi-Knockdown' ELSE '' END = mt.NAME
+            OR CASE WHEN d.IsKnockdown = 1 THEN 'Knockdown' ELSE '' END = mt.Name
+		)
